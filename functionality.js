15d23c2492b3102a71a9682929b4516b6d92d61a
pollob debnath <pullobdebnath@gmail.com>
2026-03-08 16:03:04 +0600

    static card ready

--- functionality.js
+++ functionality.js
@@ -1,5 +1,6 @@
 const username = document.getElementById("username");
 const password = document.getElementById("password");
+//Login part
 function login() {
   console.log("hey");
   const userNameValue = username.value;
@@ -7,11 +8,16 @@ function login() {
   console.log(userNameValue, passwordValue);
   if (userNameValue === "admin") {
     if (passwordValue === "admin123") {
-      window.location.assign("/home.html");
+      window.location.assign("home.html");
     } else {
       alert("Password Invalid");
     }
   } else {
     alert("UserName Invalid");
   }
+}
+
+//load all card
+const loadAllIssue=()=>{
+    
 }
